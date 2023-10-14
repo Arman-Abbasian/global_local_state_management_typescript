ad964cb91e3c1542fcdc75e778ccff027ef9bc12
Arman Abbasian <abasian.arman@gmail.com>
2023-10-14 18:00:58 +0330

    make the AddTodo component and work on hookfile in redux-toolkit

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,11 +1,31 @@
-import { useSelector } from "react-redux"
+import { useState } from "react";
+import { addTodo } from "../features/todos/todosReducer";
+import { useAppDispatch, useAppSelector } from "../features/hook";
 
 
 function AddTodo() {
-const states=useSelector((state)=>state);
-console.log(states)
+  const [name,setName]=useState<string>("")
+const dispatch=useAppDispatch();
+const state=useAppSelector((state)=>state)
+const submitHandler=(e:React.FormEvent<HTMLFormElement>)=>{
+  if(!name) return;
+  e.preventDefault();
+  dispatch(addTodo({id:Date.now(),completed:false,name}))
+  setName("")
+}
+console.log(state)
   return (
-    <div>AddTodo</div>
+    <div>
+      <form className="formContainer" onSubmit={submitHandler}>
+        <div className="width">
+        <label className="blockLabel" htmlFor="title">title</label>
+        <input className="inputDesign" type="text" value={name} 
+        onChange={(e)=>setName(e.target.value)} id="title" />
+        </div>
+        <button type="submit"  className="button width">Add</button>
+      </form>
+    </div>
+
   )
 }
 
