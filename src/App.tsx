b40f9971c8c5824bc650ca2eefef0ee240eeba99
Arman Abbasian <abasian.arman@gmail.com>
2023-10-15 15:09:44 +0330

    make Todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AddTodo from "./components/AddTodo"
 import {Provider} from "react-redux"
 import store from "./features/store"
+import Todo from "./components/Todo"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
    <Provider store={store}>
         <AddTodo />
+        <Todo />
         </Provider>
       
   )
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,16 @@
+import { AiOutlineEdit,AiOutlineDelete } from "react-icons/ai";
+
 function Todo() {
   return (
-    <div>Todo</div>
+    <div className="todoContainer">
+      <div className="todoContainer_title">
+        <p>title</p>
+      </div>
+      <div className="todoContainer_icons">
+        <AiOutlineEdit className="todoContainer_icons_edit" />
+        <AiOutlineDelete className="todoContainer_icons_delete"/>
+      </div>
+    </div>
   )
 }
 
